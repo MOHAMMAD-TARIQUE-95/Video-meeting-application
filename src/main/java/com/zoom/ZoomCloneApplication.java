80b80576bf5159c4ce67ecc9ad41104895238a62
MOHAMMAD-TARIQUE-95 <mhdtrq80@gmail.com>
2024-10-16 15:14:49 +0530

    Env variable support

--- src/main/java/com/zoom/ZoomCloneApplication.java
+++ src/main/java/com/zoom/ZoomCloneApplication.java
@@ -1,10 +1,12 @@
 package com.zoom;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ZoomCloneApplication {
+    Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
     public static void main(String[] args) {
         SpringApplication.run(ZoomCloneApplication.class, args);
     }
